645015019d62748c46e42807e02fefb00cc7a041
priteshgholap <pngholap2004@gmail.com>
2026-07-04 19:14:03 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+const fs=require('fs');
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+    fs.readdir(`./files`,function(err,files){
+        console.log(files);
+        res.render("index",{files:files});
+    })
+})
+
+app.post("/create", function (req, res) {
+    // console.log("post hit");
+    // console.log(req.body);
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,function(err){
+        if(err) console.log(err);
+        else console.log("file created");
+        res.redirect("/");
+    });
+});
+
+
+app.listen(3000,function(){
+    console.log("running...");
+});
